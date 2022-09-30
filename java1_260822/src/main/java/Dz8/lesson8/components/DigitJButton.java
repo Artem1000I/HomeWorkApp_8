ee57483303a395c3542750f5be54a1154e9f9c4e
Artem1000I <filolog999333@mail.ru>
2022-09-30 23:08:13 +1000

    Домашнее задание номер 8

---
+++ java1_260822/src/main/java/Dz8/lesson8/components/DigitJButton.java
@@ -0,0 +1,13 @@
+package Dz8.lesson8.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class DigitJButton extends JButton {
+    public DigitJButton(String text) {
+        super(text);
+        setFont(new Font("TimesRoman", Font.PLAIN, 25));
+        setBackground(new Color(70, 159, 231));
+    }
+}
